52af8f8bfadb8fd2464da88fc8e49415a8cd04d1
renu100 <renu.chandraker08@gmail.com>
2021-09-10 11:53:57 +0530

    Added:Calculate part time empwage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,6 +1,8 @@
 #! /bin/bash -x
 #constant variable
+echo "doing changes on remote"
 IS_PRESENT=1
+IS_PRESENT_HALF_TIME=2
 EMP_RATE_PER_HR=20
 
 #variab
@@ -11,8 +13,13 @@ if [ $empCheck -eq $IS_PRESENT ]
 then 
       echo "Employe is present"
            empHrs=8
-        dailywage=$(( empHrs * EMP_RATE_PER_HR ))
-else 
-       echo "Employe is absent"
-          dailywage=0
+elif [ $empCheck -eq $IS_PRESENT_HALF_TIME ]
+then
+     empHrs=8
+else
+    echo "absent"
+      empHrs=0
 fi
+echo "dailywage=$((empHrs * EMP_RATE_PER_HR))"
+
+

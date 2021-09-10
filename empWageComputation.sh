d7ead965bbb98018d045dacd067a357f06908e1c
renu100 <renu.chandraker08@gmail.com>
2021-09-10 11:34:58 +0530

    Wage computation added for employee

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,2 +1,17 @@
 #! /bin/bash -x
 echo "Welcome to employee wage attendance system"
+
+
+#constant variable
+IS_PRESENT=1
+empcheck=$((RANDOM%2))
+
+if [ $empCheck -eq $IS_PRESENT ]
+then
+echo "Employee is present"
+else
+echo "Employee is Absent"
+fi
+
+
+
